191a54e7c1ebed23fd27693b7bb22492b3664e61
doktor alban <doktoralban@outlook.com>
2020-07-06 21:30:04 +0300

    Initial commit

---
+++ TestDataTable/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestDataTable
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TestDataTable/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System.Web.Mvc;
+
+namespace TestDataTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+        public ActionResult Index4()
+        {
+            return View();
+        }
+        public ActionResult Index5()
+        {
+            return View();
+        }
+        public ActionResult Index6()
+        {
+            return View();
+        }
+        public ActionResult Index7()
+        {
+            return View();
+        }
+        public ActionResult Index8()
+        {
+            return View();
+        }
+        public ActionResult Index9()
+        {
+            return View();
+        }
+
+        public ActionResult Index10()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
